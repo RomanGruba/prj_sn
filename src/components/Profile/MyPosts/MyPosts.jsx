dc6011bf4ba876049f7655cb1c2e1ea6ef5fd085
Roman Gruba <roman.gruba@gmail.com>
2019-12-19 00:33:42 +0200

    n

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,17 +4,18 @@ import Post from "./Post/Post";
 
 const MyPosts = props => {
   let newPostElement = React.createRef();
-  let text = "";
-  if (newPostElement) {
-    text = newPostElement.current.value;
-  }
+
+  let addPost = () => {
+    let newPostText = newPostElement.current.value;
+    props.addPost(newPostText);
+  };
   return (
     <div className={styles.posts}>
       <h3> My posts</h3>
       <div>
         <textarea ref={newPostElement}></textarea>
         <div className={styles.addPostButton}>
-          <button onClick={props.addPost(text)}>Add post</button>
+          <button onClick={addPost}>Add post</button>
         </div>
       </div>
       {props.posts.map(post => (

595c22c900c76004d90e8196cad69f6c8205629b
Roman Gruba <roman.gruba@gmail.com>
2019-12-21 19:20:45 +0200

    n

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
-import { addPost } from "./redux/state";
-import App from "./App";
+import { rerenderEntireTree } from "./render";
 import state from "./redux/state";
 
-ReactDOM.render(
-  <App state={state} addPost={addPost} />,
-  document.getElementById("root")
-);
+rerenderEntireTree(state);
